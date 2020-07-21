81a27365570475674ffb3e46389445ea66ae9bf5
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-21 19:42:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -26,7 +26,9 @@ def nyc_pigeon_organizer(data)
     data[key].each do |inner_key, inner_value|
 #      p inner_key
       data[key][inner_key].each do |pigeon_name|
-        p pigeon_list[pigeon_name][key]
+        pigeon_list[pigeon_name] = {}
+        pigeon_list[pigeon_name][key] = []
+        pigeon_list[pigeon_name][key].push(inner_key.to_s)
       end
       
     end
